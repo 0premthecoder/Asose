6d0962f1c4bfe6ec7fba4ff46e6ef1c61f98977e
Prem kumar <70461546+prem0556@users.noreply.github.com>
2023-01-05 17:13:22 +0000

    show name on dashboard

--- pages/Myaccount.js
+++ pages/Myaccount.js
@@ -4,51 +4,50 @@ import DashboardHeader from './components/DashboardHeader'
 import styles from './../styles/Dashboard.module.css'
 import Overview from './components/Overview'
 var jwt = require('jsonwebtoken');
+import { useEffect } from "react";
+
 function Myaccount() {
   const [active, setActive] = useState('o')
   const [name, setName] = useState('user')
-  const getname=()=>{
-    try{
-    let token = localStorage.getItem('token')
-    
-    let dec = jwt.decode(token);
+
+  useEffect(() => {
+    // Perform localStorage action
+    const token = localStorage.getItem('token')
+    let dec = jwt.decode(token)
     setName(dec.name)
-    }
-    catch{
-      console.log('mu')
-    }
-  }
-    return (
-      <>
-      
-        <DashboardHeader />
-        <div className={styles.main}>
-          <img id='logo' src="/home.jpg" alt="logo" className={styles.homeImage} width={300} height={200} />
-          <h1>
-            &lt;Welcome&#47;&gt;
-          </h1>
-          <h2 style={{ "color": '#0eb582' }} onClick={getname()}>{name}</h2>
+  }, [])
 
-          <div className={styles.grid}>
-            <h4 className={active === 'o' ? styles.active : ''} onClick={() => setActive('o')}>Overview</h4>
-            <h4 className={active === 't' ? styles.active : ''} onClick={() => setActive('t')}>Todos</h4>
-            <h4 className={active === 'a' ? styles.active : ''} onClick={() => setActive('a')}>Announcement</h4>
-          </div>
-          {active === 'o' ? <Overview /> : ''}
-          {active === 'a' ? <h3>coming soon</h3> : ''}
-          {active === 't' ? <h3>We are Working on it</h3> : ''}
+  return (
+    <>
+
+      <DashboardHeader />
+      <div className={styles.main}>
+        <img id='logo' src="/home.jpg" alt="logo" className={styles.homeImage} width={300} height={200} />
+        <h1>
+          &lt;Welcome&#47;&gt;
+        </h1>
+        <h2 style={{ "color": '#0eb582' }}>{name}</h2>
+
+        <div className={styles.grid}>
+          <h4 className={active === 'o' ? styles.active : ''} onClick={() => setActive('o')}>Overview</h4>
+          <h4 className={active === 't' ? styles.active : ''} onClick={() => setActive('t')}>Todos</h4>
+          <h4 className={active === 'a' ? styles.active : ''} onClick={() => setActive('a')}>Announcement</h4>
         </div>
+        {active === 'o' ? <Overview /> : ''}
+        {active === 'a' ? <h3>coming soon</h3> : ''}
+        {active === 't' ? <h3>We are Working on it</h3> : ''}
+      </div>
 
-        <footer className={styles.footer}>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Created by &#9998;&nbsp;<span style={{ fontWeight: 'bolder' }}>Coding Team</span>
-          </a>
-        </footer>
-      </>
-    )
-  }
+      <footer className={styles.footer}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by &#9998;&nbsp;<span style={{ fontWeight: 'bolder' }}>Coding Team</span>
+        </a>
+      </footer>
+    </>
+  )
+}
 
 export default Myaccount

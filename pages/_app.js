9da5c1fe98c99b9d53172baaa23ac13d72b4bb28
Prem kumar <70461546+prem0556@users.noreply.github.com>
2023-01-06 16:25:07 +0000

    _hydrateapp

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from 'react';
 function MyApp({ Component, pageProps }) {
-
+  const [hydrated, setHydrated] = React.useState(false);
+  React.useEffect(() => {
+      setHydrated(true);
+  }, []);
+  if (!hydrated) {
+      // Returns null on first render, so the client and server match
+      return null;
+  }
   return <Component {...pageProps} />
 }
 

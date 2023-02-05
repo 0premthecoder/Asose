971d295aaaee8aab9d43c5be7eb2f3f712fa3274
Prem kumar <70461546+prem0556@users.noreply.github.com>
2023-02-05 12:34:54 +0000

    fixedbug

--- pages/index.js
+++ pages/index.js
@@ -30,16 +30,16 @@ function Index() {
         draggable: true,
         progress: undefined,
         theme: "dark",
-      });
+    });
 
     return (
         // <React.Fragment>
         <>
 
-      <Head>
-        <title>Asose Sector 5 Dwarka</title>
-        <meta name="description" content="Fuck You Fuck off" />
-      </Head>
+            <Head>
+                <title>Asose Sector 5 Dwarka</title>
+                <meta name="description" content="Fuck You Fuck off" />
+            </Head>
             {/* account form section ends */}
             {/* header section ends */}
             {/* home section starts  */}
@@ -47,9 +47,22 @@ function Index() {
             <HomeHeader />
             {/* home section ends */}
             {/* subjects section starts  */}
-            
+
             {/* subjects section ends */}
             {/* home courses slider section starts  */}
+
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
             <section className="home-courses .home-courses-slider" >
                 <h1 className="heading"> Our Popular Skills </h1>
                 <Swiper className="swiper home-courses-slider" pagination={{
@@ -68,16 +81,16 @@ function Index() {
                     <div className="swiper-wrapper">
                         <SwiperSlide className="swiper-slide slide">
                             <div className="image">
-                                <Image src="/Course1-1.png" 
-                                width={400}
-                                height={300}
-                                 alt="coding" />
+                                <Image src="/Course1-1.png"
+                                    width={400}
+                                    height={300}
+                                    alt="coding" />
                                 <h3>Coding</h3>
                             </div>
                             <div className="content">
                                 <h3>Coding</h3>
                                 <p>
-                                Coding is the process of writing instructions to a computer in a language that it can understand and execute...
+                                    Coding is the process of writing instructions to a computer in a language that it can understand and execute...
                                 </p>
                                 <Link href="/blog/coding" className={styles.btn}>
                                     read more
@@ -87,13 +100,13 @@ function Index() {
                         <SwiperSlide className="swiper-slide slide">
                             <div className="image">
                                 <Image width={400}
-                                height={300}  src="/emp2.png" alt="" />
+                                    height={300} src="/emp2.png" alt="" />
                                 <h3>Electro Mechanical &amp;Production</h3>
                             </div>
                             <div className="content">
                                 <h3>Electro Mechanical &amp; Production</h3>
                                 <p>
-                                The process of creating products using electrical and mechanical components to assemble parts, materials, and systems...
+                                    The process of creating products using electrical and mechanical components to assemble parts, materials, and systems...
                                 </p>
                                 <Link href="/blog/electro-mechanical-production" className={styles.btn}>
                                     read more
@@ -103,13 +116,13 @@ function Index() {
                         <SwiperSlide className="swiper-slide slide">
                             <div className="image">
                                 <Image width={400}
-                                height={300} src="/finance.png" alt="" />
+                                    height={300} src="/finance.png" alt="" />
                                 <h3>Finance &amp; Accounting</h3>
                             </div>
                             <div className="content">
                                 <h3>Finance &amp; Accounting</h3>
                                 <p>
-                                Finance and accounting involve the management of money and resources through budgeting, forecasting, and analyzing financial data...
+                                    Finance and accounting involve the management of money and resources through budgeting, forecasting, and analyzing financial data...
                                 </p>
                                 <Link href="/blog/finance" className={styles.btn}>
                                     read more
@@ -119,13 +132,13 @@ function Index() {
                         <SwiperSlide className="swiper-slide slide">
                             <div className="image">
                                 <Image width={400}
-                                height={300} src="/robotics.png" alt="" />
+                                    height={300} src="/robotics.png" alt="" />
                                 <h3>Robotics</h3>
                             </div>
                             <div className="content">
                                 <h3>Robotics</h3>
                                 <p>
-                                Robotics is the engineering science and technology of creating robots and systems for automation and performing complex tasks...
+                                    Robotics is the engineering science and technology of creating robots and systems for automation and performing complex tasks...
                                 </p>
                                 <Link href="/blog/robotics" className={styles.btn}>
                                     read more
@@ -135,13 +148,13 @@ function Index() {
                         <SwiperSlide className="swiper-slide slide">
                             <div className="image">
                                 <Image width={400}
-                                height={300} src="/fashion bg.png" alt="" />
+                                    height={300} src="/fashion bg.png" alt="" />
                                 <h3>Fashion Design And Aesthetics</h3>
                             </div>
                             <div className="content">
                                 <h3>Fashion Design And Aesthetics</h3>
                                 <p>
-                                Creating unique garments and accessories that express personal style and reflect current trends...
+                                    Creating unique garments and accessories that express personal style and reflect current trends...
                                 </p>
                                 <Link href="/blog/fashion" className={styles.btn}>
                                     read more
@@ -151,21 +164,21 @@ function Index() {
                         <SwiperSlide className="swiper-slide slide">
                             <div className="image">
                                 <Image width={400}
-                                height={300} src="/dmd1.png" alt="" />
+                                    height={300} src="/dmd1.png" alt="" />
                                 <h3>Digital Media &amp; Design</h3>
                             </div>
                             <div className="content">
                                 <h3>Digital Media &amp; Design</h3>
                                 <p>
-                                Digital Media & Designing is the art of creating digital content and visual designs to communicate ideas and messages through digital media...
+                                    Digital Media & Designing is the art of creating digital content and visual designs to communicate ideas and messages through digital media...
                                 </p>
                                 <Link href="blog/digital-media-and-designing" className={styles.btn}>
                                     read more
                                 </Link>
                             </div>
                         </SwiperSlide>
-                        
-                        
+
+
                     </div>
                 </Swiper>
             </section>

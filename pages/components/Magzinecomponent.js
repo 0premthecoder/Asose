49e58b5c4f6d1fb998a176bce53bcf8a0f48b048
Prem kumar <70461546+prem0556@users.noreply.github.com>
2023-02-05 12:27:26 +0000

    magzinesuccess

---
+++ pages/components/Magzinecomponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Script from 'next/script'
+export default function Magzinecomponent() {
+  return (
+    <div><a href="https://online.flippingbook.com/view/651093132/" className="fbo-embed" data-fbo-id="17d71b5e67" data-fbo-ratio="3:2" data-fbo-lightbox="yes" data-fbo-width="100%" data-fbo-height="auto" data-fbo-version="1" style={{maxWidth: "100%"}}>magzine </a>
+    <Script src="https://online.flippingbook.com/EmbedScriptUrl.aspx?m=redir&hid=651093132"></Script></div>
+  )
+}
+
+
+
--- pages/magzine.js
+++ pages/magzine.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import Script from 'next/script'
 import { useEffect } from 'react'
 import  Router from 'next/router'
+import Magzinecomponent from './components/Magzinecomponent'
 export default function magzine() {
-    
+    const [login, setLogin] = useState(false)
     useEffect(() => {
         // Perform localStorage action
         if(localStorage.getItem('mail')){
-            console.log("Logined")
+            setLogin(true)
         }
         else{
             Router.push('/')
+            setLogin(false)
         }
       }, [])
-    return (
-    <>
-
-    <a href="https://online.flippingbook.com/view/651093132/" className="fbo-embed" data-fbo-id="17d71b5e67" data-fbo-ratio="3:2" data-fbo-lightbox="yes" data-fbo-width="100%" data-fbo-height="auto" data-fbo-version="1" style={{maxWidth: "100%"}}>magzine </a>
-    <Script src="https://online.flippingbook.com/EmbedScriptUrl.aspx?m=redir&hid=651093132"></Script>
-    </>
-    )
+      if(login){
+        return <Magzinecomponent/>
+      }else{
+        return <h1>Please Login</h1>
+      }
+    
 }

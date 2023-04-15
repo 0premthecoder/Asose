65060d93507ad8266cf20e1aa8d4609d8138ce8d
Prem kumar <70461546+prem0556@users.noreply.github.com>
2023-04-15 08:04:00 +0000

    magzine

--- pages/components/Magzinecomponent.js
+++ pages/components/Magzinecomponent.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Script from 'next/script'
 export default function Magzinecomponent() {
   return (
     <iframe frameborder="0" allowtransparency="true" allowFullscreen="true" style="width: 100%; height: 480px; border: none;" src="https://online.visual-paradigm.com/share/book/magzine-compressed-1bnm9vq54i?p=1"></iframe>
--- pages/magzine.js
+++ pages/magzine.js
@@ -4,21 +4,22 @@ import  Router from 'next/router'
 import Magzinecomponent from './components/Magzinecomponent'
 
 export default function magzine() {
-    const [login, setLogin] = useState(false)
-    useEffect(() => {
-        // Perform localStorage action
-        if(localStorage.getItem('mail')){
-            setLogin(true)
-        }
-        else{
-            Router.push('/')
-            setLogin(false)
-        }
-      }, [])
-      if(login){
-        return <Magzinecomponent/>
-      }else{
-        return <h1>Please Login</h1>
-      }
+    // const [login, setLogin] = useState(false)
+    // useEffect(() => {
+    //     // Perform localStorage action
+    //     if(localStorage.getItem('mail')){
+    //         setLogin(true)
+    //     }
+    //     else{
+    //         Router.push('/')
+    //         setLogin(false)
+    //     }
+    //   }, [])
+    //   if(login){
+    //     return <Magzinecomponent/>
+    //   }else{
+    //     return <h1>Please Login</h1>
+    //   }
+    <Magzinecomponent/>
     
 }

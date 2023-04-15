b71f9e41bd5a1e238826e47ce3b7979ff3b3f7e6
Prem kumar <70461546+prem0556@users.noreply.github.com>
2023-04-15 13:04:26 +0530

    Update DashboardHeader.js

--- pages/components/DashboardHeader.js
+++ pages/components/DashboardHeader.js
@@ -24,7 +24,7 @@ export default function DashboardHeader() {
             <nav className={`navbar ${navactive}`} >
                 <div id="close-navbar" className="fas fa-times" onClick={() => setNavactive('')} />
                 <Link href='/Myaccount'><i className="fa fa-user-secret" aria-hidden="true"></i></Link>
-                <Link href="/magzine">Magzine</Link>
+                <Link href="https://www.canva.com/design/DAFYQx5L0DQ/vSVkOgr0sGKeE-BQjtVn6Q/view">Magzine</Link>
                 <Link href="/"><i className="fa fa-user-times" aria-hidden="true"></i></Link>
                 {/* <Link href="/contact">Contact</Link> */}
             </nav>

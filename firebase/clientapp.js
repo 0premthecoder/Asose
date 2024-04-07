0d45f5cf5ca8759c42035514401fb605e9a9cb3b
Shubham <105529358+Shubh99992@users.noreply.github.com>
2024-04-07 12:14:20 +0000

    Firebase fix1

--- firebase/clientapp.js
+++ firebase/clientapp.js
@@ -1,10 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
-
-
-
 
 const clientCredentials = {
   apiKey: "AIzaSyAUxcVIH1VlP0fXFvYVo1RbypJz0173Fao",
@@ -15,7 +12,6 @@ const clientCredentials = {
   appId: "1:977548250136:web:25dd08c8ae8d4717e3c76d",
   measurementId: "G-5TR4WGS8TT"
 };
-
 
 export const firebase = initializeApp(clientCredentials);
 export const db = getFirestore(firebase);
